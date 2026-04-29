eb6de264d819e921a146311d99a5d299a9276223
slxu <slxu.phys@gmail.com>
2026-04-29 11:01:11 -0500

    Revert "Feat: paged.js page mode with debounced re-render"
    This reverts commit 82add385cd9a598e70be2e5bdeaa4e86ee9f9cee.

--- src/previewViewProvider.ts
+++ src/previewViewProvider.ts
@@ -390,7 +390,6 @@ ${renderedHtml}
     const scriptUri = webview.asWebviewUri(vscode.Uri.joinPath(this.extensionUri, "media", "main.js"));
     const mdRendererUri = webview.asWebviewUri(vscode.Uri.joinPath(this.extensionUri, "media", "markdown-renderer.js"));
     const styleUri = webview.asWebviewUri(vscode.Uri.joinPath(this.extensionUri, "media", "styles.css"));
-    const pagedScriptUri = webview.asWebviewUri(vscode.Uri.joinPath(this.extensionUri, "media", "paged.min.js"));
     const katexScriptUri = webview.asWebviewUri(vscode.Uri.joinPath(this.extensionUri, "media", "katex.min.js"));
     const katexStyleUri = webview.asWebviewUri(vscode.Uri.joinPath(this.extensionUri, "media", "katex.min.css"));
     const hlScriptUri = webview.asWebviewUri(vscode.Uri.joinPath(this.extensionUri, "media", "highlight.min.js"));
@@ -428,12 +427,6 @@ ${renderedHtml}
         <path fill-rule="evenodd" d="M5 11.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm-3 1a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm0 4a1 1 0 1 0 0-2 1 1 0 0 0 0 2zm0 4a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"/>
       </svg>
     </button>
-    <button id="togglePageModeButton" type="button" title="Toggle page mode" aria-pressed="false">
-      <svg width="14" height="14" viewBox="0 0 16 16" fill="currentColor" aria-hidden="true">
-        <path d="M5 4a.5.5 0 0 0 0 1h6a.5.5 0 0 0 0-1H5zm-.5 2.5A.5.5 0 0 1 5 6h6a.5.5 0 0 1 0 1H5a.5.5 0 0 1-.5-.5zM5 8a.5.5 0 0 0 0 1h6a.5.5 0 0 0 0-1H5zm0 2a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1H5z"/>
-        <path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2zm10-1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h8a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1z"/>
-      </svg>
-    </button>
     <button id="fetchBibButton" type="button" title="Fetch bibliography from CrossRef / arXiv">
       <svg width="14" height="14" viewBox="0 0 16 16" fill="currentColor" aria-hidden="true">
         <path d="M1 2.828c.885-.37 2.154-.769 3.388-.893 1.33-.134 2.458.063 3.112.752v9.746c-.935-.53-2.12-.603-3.213-.493-1.18.12-2.37.461-3.287.811zm7.5-.141c.654-.689 1.782-.886 3.112-.752 1.234.124 2.503.523 3.388.893v9.923c-.918-.35-2.107-.692-3.287-.81-1.094-.111-2.278-.039-3.213.492zM8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783"/>
@@ -453,11 +446,9 @@ ${renderedHtml}
     <section id="emptyState" class="empty-state">Open a notebook to preview cells.</section>
     <section id="cellList" class="cell-list" aria-label="Notebook preview cells"></section>
     <section id="referenceList" class="reference-list" hidden></section>
-    <div id="pagedOutput" hidden></div>
   </main>
   <script nonce="${nonce}" src="${katexScriptUri}"></script>
   <script nonce="${nonce}" src="${hlScriptUri}"></script>
-  <script nonce="${nonce}" src="${pagedScriptUri}"></script>
   <script nonce="${nonce}">
     window.__NOTEBOOK_PREVIEW_CONFIG__ = {
       followActiveCell: ${this.followActiveCell ? "true" : "false"}
